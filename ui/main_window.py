50a12d220d0f98e78059ee35626e36230b3ea5ef
Corentin Bausset <corentin.bausset@gmail.com>
2025-12-23 20:17:15 +0100

    Changements graphiques au clic du bouton.

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 
-def hello_world():
-    print("Hello World.")
+
 
 
 def launch_app():
     root = tk.Tk()
     root.title("MCI - Meross Controller Interface")
     root.geometry("400x300")
+    
+    def hello_world():
+        btn_1.config(text="Cliqué !",state="disabled")
     
     btn_1 = tk.Button(root,text="Hello World", command=hello_world)
     
