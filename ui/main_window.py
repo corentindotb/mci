88d4486ac4216de6c5f020755bf5222fe17e3cb0
Corentin Bausset <corentin.bausset@gmail.com>
2025-12-23 20:09:09 +0100

    Premier boutton avec command function (Hello World).

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 
+def hello_world():
+    print("Hello World.")
+
+
 def launch_app():
     root = tk.Tk()
     root.title("MCI - Meross Controller Interface")
     root.geometry("400x300")
+    
+    btn_1 = tk.Button(root,text="Hello World", command=hello_world)
+    
+    btn_1.pack()
     root.mainloop()
